ec7df2e837bea50318add14558d2c2ebd888ba86
zafiragalea <87572241+zafiragalea@users.noreply.github.com>
2024-07-23 00:00:58 +0700

    local8000

--- src/Redux/ActionCreater.js
+++ src/Redux/ActionCreater.js
@@ -16,7 +16,7 @@ export const GetAllAccounts = () => {
     setTimeout(() => {
       // console.log('Fetching data...');
       axios
-        .get('https://users-api-beryl.vercel.app/api/user')
+        .get('http://localhost:8000/user')
         .then((res) => {
           // console.log('Response received:', res);
           const _list = res.data;
@@ -35,7 +35,7 @@ export const CreateUser = (userlist) => {
     dispatch(makeRequest());
     // console.log('Fetching data...');
     axios
-      .post('https://users-api-beryl.vercel.app/api/user', userlist)
+      .post('http://localhost:8000/user', userlist)
       .then(() => {
         // console.log('Response received:', res);
         //   const _list = res.data;
@@ -55,7 +55,7 @@ export const GetUserbyCode = (code) => {
     //   dispatch(makeRequest());
     // console.log('Fetching user by code');
     axios
-      .get('https://users-api-beryl.vercel.app/api/user/' + code)
+      .get('http://localhost:8000/user/' + code)
       .then((res) => {
         // console.log('Response received:', res);
         const _obj = res.data;
@@ -74,10 +74,7 @@ export const EditUser = (userlist) => {
     dispatch(makeRequest());
     // console.log('Fetching data...');
     axios
-      .put(
-        'https://users-api-beryl.vercel.app/api/user/' + userlist.id,
-        userlist
-      )
+      .put('http://localhost:8000/user/' + userlist.id, userlist)
       .then(() => {
         // console.log('Response received:', res);
         //   const _list = res.data;
